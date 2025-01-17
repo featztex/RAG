7b5765e29e81c0c527ee3c8a8a9ce01771dbb18a
featztex <romaem3019@gmail.com>
2025-01-17 19:33:59 +0300

    test response

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-# comment
+import os
+from mistralai import Mistral
+from config import api_key
+
+key = api_key
+model = "mistral-large-latest"
+client = Mistral(api_key=key)
+
+test_response = client.chat.complete(
+    model=model,
+    messages=[
+        {
+            "role": "user",
+            "content": "What is the capital of Romania?"
+        }
+    ]
+)
+
+print(test_response.choices[0].message.content)
