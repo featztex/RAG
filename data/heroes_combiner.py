b6e37b878d15f52f0a96dab7d20fe11c1f6d8385
featztex <romaem3019@gmail.com>
2025-01-19 18:13:59 +0300

    concat all info

---
+++ data/heroes_combiner.py
@@ -0,0 +1,19 @@
+﻿import os
+
+folder_path = 'data/game_of_thrones_heroes'
+output_file = 'data/content.txt'
+
+files = [f for f in os.listdir(folder_path) if f.endswith('.txt')]
+files.sort()
+biographies = []
+
+for file in files:
+    hero_name = file.replace('.txt', '')
+    with open(os.path.join(folder_path, file), 'r', encoding='utf-8') as f:
+        content = f.read()
+    biographies.append(f"{hero_name}\n\n{content}")
+
+final_content = '\n\n\n'.join(biographies)
+
+with open(output_file, 'w', encoding='utf-8') as f:
+    f.write(final_content)
