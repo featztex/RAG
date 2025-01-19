80ece9498ac447d82a300860776b2b180310fd1a
featztex <romaem3019@gmail.com>
2025-01-19 19:39:35 +0300

    reduced

--- dialogue.py
+++ dialogue.py
@@ -1,14 +1,3 @@
-﻿from main import RAG_pipline, ask_rag
+﻿from main import start_dialogue
 
-qa_chain = RAG_pipline()
-
-print("RAG-система готова. Введите вопрос или 'выход' для завершения.")
-while True:
-    user_input = input("Ваш вопрос: ")
-    if user_input.lower() == 'выход':
-        break
-    answer, sources = ask_rag(user_input, qa_chain)
-    print(f"Ответ: {answer}\n")
-    print('Источники:')
-    for source in sources:
-        print(f"- ...{source.page_content[:200]}...\n")
+start_dialogue()
